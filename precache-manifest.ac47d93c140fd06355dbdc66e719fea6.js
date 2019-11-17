3e47e2bd3c177e1b018d6e3c3b5c1749282571e1
zikwall <andrey.kapitonov.96@gmail.com>
2019-11-17 18:50:16 +0300

    Updates

---
+++ precache-manifest.ac47d93c140fd06355dbdc66e719fea6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2821a096901e7fb2c54f510b80502e19",
+    "url": "/vk-tetris-react/index.html"
+  },
+  {
+    "revision": "9125081fd37c27a383f9",
+    "url": "/vk-tetris-react/static/css/2.266626f9.chunk.css"
+  },
+  {
+    "revision": "a16219a49cdcd38576f8",
+    "url": "/vk-tetris-react/static/css/main.20caadca.chunk.css"
+  },
+  {
+    "revision": "9125081fd37c27a383f9",
+    "url": "/vk-tetris-react/static/js/2.b5c61ceb.chunk.js"
+  },
+  {
+    "revision": "a16219a49cdcd38576f8",
+    "url": "/vk-tetris-react/static/js/main.c8af1721.chunk.js"
+  },
+  {
+    "revision": "2c7783f314518707fb26",
+    "url": "/vk-tetris-react/static/js/runtime-main.7b9ac37a.js"
+  },
+  {
+    "revision": "ec7fc8bd9ef63900b178c646cbc4c3fb",
+    "url": "/vk-tetris-react/static/media/Tetris.ec7fc8bd.woff"
+  }
+]);
