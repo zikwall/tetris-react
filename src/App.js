263148fd99b5baeedaf6dfec6bdde129b24ff551
zikwall <andrey.kapitonov.96@gmail.com>
2019-11-23 18:25:25 +0300

    :art: Change theme color meta && app styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import { View, Panel, PanelHeader, PanelHeaderContent, HeaderButton, platform, IOS } from '@vkontakte/vkui';
+import { View, Panel, PanelHeader, PanelHeaderContent } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 import './assets/css/app.css';
-import Icon28Menu from '@vkontakte/icons/dist/28/menu';
-import Icon24Services from '@vkontakte/icons/dist/24/services';
-import Icon36Article from '@vkontakte/icons/dist/36/article';
 
 import Tetris from "./Tetris";
 
 function App() {
-    const osname = platform();
-
     return (
         <View activePanel="main">
             <Panel id="main">
-                <PanelHeader left={
-                    <HeaderButton>
-                        {
-                            osname === IOS ? <Icon28Menu /> : <Icon36Article />
-                        }
-                    </HeaderButton>
-                }>
+                <PanelHeader>
                     <PanelHeaderContent>
                         Tetris
                     </PanelHeaderContent>
